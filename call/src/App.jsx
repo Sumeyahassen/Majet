efddc6d88586e2f07c8f85b5632d1dbf6a267c28
sumeyahassen <sumeyahassensumi@gmail.com>
2025-09-30 09:39:59 +0300

    start the button gui

---
+++ call/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Buttans from './components/Buttans'
+
+function App() {
+  return (
+    <div>
+      <div className=" text-green-600  text-lg">
+        hellow ubuntu
+      </div>
+      <Buttans/>
+    </div>
+  )
+}
+
+export default App
---
+++ call/src/components/Buttans.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Buttans() {
+  return (
+    <div className='bg-gray-600 text-white grid grid-cols-3 py-11 mx-96 max-w-md justify-between' >
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold" >1</button>
+   <button className="p-4  bg-black text-white m-4 rounded-xl text-lg font-semibold">2</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">3</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">4</button>
+   <button className="p-4 bg-black text-white  m-4 rounded-xl text-lg font-semibold">5</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">6</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">7</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">8</button>
+   <button className="p-4 bg-black text-white  m-4 rounded-xl text-lg font-semibold">9</button>
+   <button className="p-4 bg-black text-white  m-4 rounded-xl text-lg font-semibold">*</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">0</button>
+   <button className="p-4 bg-black text-white m-4 rounded-xl text-lg font-semibold">#</button>
+    </div>
+  )
+}
+
+export default Buttans
