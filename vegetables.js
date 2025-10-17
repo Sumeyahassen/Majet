d4b217a9201112ab571939532e6796672364ea25
Eden Sahlie <eduedensahle@gmail.com>
2025-10-16 22:53:44 -0700

    majet web with html,css&js

---
+++ vegetables.js
@@ -0,0 +1,6 @@
+document.getElementById("signup").addEventListener("click", function(){
+    window.location.href="signupform.html";
+})
+document.getElementById("login").addEventListener("click", function(){
+    window.location.href="loginform.html";
+})
