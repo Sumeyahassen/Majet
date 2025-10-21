cdfa0921aea3997f8f948979ff70416ee5564885
Eden Sahlie <eduedensahle@gmail.com>
2025-10-21 02:07:13 -0700

    changes in the home, login and signup pages

--- majet.js
+++ majet.js
@@ -4,3 +4,19 @@ document.getElementById("signup").addEventListener("click", function(){
 document.getElementById("login").addEventListener("click", function(){
     window.location.href="loginform.html";
 })
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const product = document.querySelector("#product");
+const contact = document.querySelector("#contact");
+home.addEventListener("click", function(){
+    document.getElementById("homeSection").scrollIntoView({ behavior: "smooth" });
+})
+    about.addEventListener("click", function(){
+        document.getElementById("aboutSection").scrollIntoView({ behavior: "smooth" });
+})
+product.addEventListener("click", function(){
+    document.getElementById("productSection").scrollIntoView({ behavior: "smooth" });
+})
+contact.addEventListener("click", function(){
+    document.getElementById("contactSection").scrollIntoView({ behavior: "smooth" });
+})
