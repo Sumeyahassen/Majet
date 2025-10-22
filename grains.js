d3346b61fc2a5064e431595510f8b60dfc6ca358
Eden Sahlie <eduedensahle@gmail.com>
2025-10-22 09:15:38 -0700

    finishing to push to github

--- grains.js
+++ grains.js
@@ -4,15 +4,17 @@ document.getElementById("login").addEventListener("click", function(){
 document.getElementById("signup").addEventListener("click", function(){
     window.location.href="signupform.html";
 })
-
 const buttons = document.querySelectorAll(".container .grains .cart");
+const prices = document.querySelectorAll(".container .grains .price");
 buttons.forEach(button=>{
     button.addEventListener("click", function(){
         const par = button.parentElement;
         const parent = this.closest('.grains');        
         button.remove();
+        const pricePar = par.querySelector(".price");
+        const price = parseFloat(pricePar.dataset.price);
         const quantitiy = document.createElement('label');
-        quantitiy.textContent = "Enter amount: ";
+        quantitiy.textContent = "Enter quantitiy: ";
         const input = document.createElement('input');
         input.value = '1';
         input.min = '1';
@@ -26,8 +28,10 @@ buttons.forEach(button=>{
         par.appendChild(save);
         par.appendChild(cancel);
         save.addEventListener("click", function(){
-            if(input.value>=1){
-                alert('You have added '+input.value+' kg '+parent.id+' to your cart');
+            if(input.value >= 1){
+                const value = parseFloat(input.value);
+                const Total = value * price;
+                alert("You have added "+value+" kg of "+parent.id+"  to your cart and it's total price is "+Total+" .");
             }
             else{
                 alert('Please enter a valid amount greater than or equal to 1!')
--- vegetables.js
+++ vegetables.js
@@ -5,20 +5,22 @@ document.getElementById("login").addEventListener("click", function(){
     window.location.href="loginform.html";
 })
 const conatiners = document.querySelectorAll(".container .vegetable .cart");
+const prices = document.querySelectorAll(".container .ve .price")
 conatiners.forEach(button=>{
     button.addEventListener("click", function(){
         const par = button.parentElement;
         const parent = this.closest('.vegetable');
         button.remove();
+        const pricepar = par.querySelector(".price");
+        const price = parseFloat(pricepar.dataset.price);
         const quantitiy = document.createElement("label");
-        quantitiy.textContent = "Enter quantity";
+        quantitiy.textContent = "Enter quantity:";
         const input = document.createElement('input');
         input.min = '1';
         input.value = '1';
         input.type = 'number';
-        quantitiy.appendChild(input);
-        par.insertAdjacentElement('beforeend',quantitiy);
-    
+        par.appendChild(quantitiy);
+        par.appendChild(input);
         const save = document.createElement('button');
         save.textContent = 'Add';
         const cancel = document.createElement('button');
@@ -27,7 +29,9 @@ conatiners.forEach(button=>{
         par.appendChild(cancel);
         save.addEventListener('click', function(){
             if(input.value>=1){
-                alert("You have added "+input.value+" kg "+parent.id+" to your cart");
+                const value = parseFloat(input.value);
+                const Total = value * price;
+                alert("You have added "+value+" kg of "+parent.id+" to your cart and it's total price is "+Total+".");
             }
             else{
                 alert("please enter a valid amount greater than or equal to 1!")
