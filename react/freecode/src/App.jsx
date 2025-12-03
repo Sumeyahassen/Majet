ae6317c3f7ecf3a6a2e42160ab149cf09cfacd93
sumeyahassen <sumeyahassensumi@gmail.com>
2025-12-03 21:53:21 +0300

    start the learning in the one file diffrent componet to  use

--- react/freecode/src/App.jsx
+++ react/freecode/src/App.jsx
@@ -1,7 +1,18 @@
+const Form = () => {
+  const ti = () => {
+    setTimeout(() => console.log("line-3"), 1000);
+  };
+  console.log("line-1");
+  ti();
+  console.log("line-2");
+  console.log("line-4");
+  return <div>Form Component</div>;
+};
 function App() {
   return (
     <div className="bg-green-400">
       <h1 className="text-black">Sumeya Hassen</h1>
+      <Form />
     </div>
   );
 }
